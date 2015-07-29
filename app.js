d570886d739dbd0a3ef21ee777962ee69a2fb5c2
OITAO <adrianosorio101@gmail.com>
2015-07-29 15:31:58 -0700

    adding in a new changed Announcements center since the bootstrap version was not appealing also centered calender and changed background for the calendar to a light blue.

---
+++ app.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    //this event states that on click of entry-title then it will slidedown and display entry-content and toggle these events
+    $(".entry").on("click", function(){
+      $(this).children(".entry-content").slideToggle().toggleClass("active-entry");
+    });
+});
